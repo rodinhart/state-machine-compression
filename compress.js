03b94f31e2417ef2748aa651114194c06b4b1e6e
Rodin van der Hart <rodinhart@gmail.com>
2017-02-12 20:34:04 +0000

    Added more debug information

--- compress.js
+++ compress.js
@@ -1,10 +1,11 @@
 "use strict"
 
 const fs = require("fs")
+const kb = x => Math.round(x / 1000) + "Kb"
 
 console.log("Reading file...")
-const source = fs.readFileSync("compress.js")
-console.log("Source length: " + source.length)
+const source = fs.readFileSync("./public/ZarchEncrypted.txt")
+console.log("Source length: " + kb(source.length))
 
 var hist, i, max, symbol
 
@@ -18,6 +19,10 @@ for (i = 0; i < source.length; i++) {
   hist[symbol]++
   if (hist[symbol] > max) max = hist[symbol]
 }
+
+console.log("Optimal: " + kb(hist.reduce((a, x) =>
+  a + (x !== 0 ? x * Math.log(source.length / x) / Math.log(2) : 0)
+  , 0) / 8))
 
 for (i = 0; i < 256; i++) {
   hist[i] = hist[i] !== 0 ? Math.round(max / hist[i]) : 0
@@ -58,9 +63,9 @@ for (i = 0; i < source.length; i++) {
 
 if (state !== 0) buffer.push(state) // 12 bits
 
-console.log("Target length: " + (buffer.length * 1.5))
+console.log("Target length: " + kb(buffer.length * 1.5))
 
-var copy, tmp
+var copy, t, tmp
 
 console.log("Decoding...")
 copy = []
@@ -73,8 +78,10 @@ for (i = 0; i < buffer.length; i++) {
     state = table[state][256]
   }
 
-  copy = copy.concat(tmp)
+  for (t = 0; t < tmp.length; t++) {
+    copy.push(tmp[t])
+  }
 }
 
-console.log("Copy length: " + copy.length)
-fs.writeFileSync("_copy.js", Buffer.from(copy))
+console.log("Copy length: " + kb(copy.length))
+fs.writeFileSync("./public/_copy.txt", Buffer.from(copy))
