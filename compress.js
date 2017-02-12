62846d73a7bfde48f6572a864f1c0d7c825f5bf2
Rodin van der Hart <rodinhart@gmail.com>
2017-02-12 19:38:40 +0000

    First attempt

--- compress.js
+++ compress.js
@@ -2,10 +2,13 @@
 
 const fs = require("fs")
 
+console.log("Reading file...")
 const source = fs.readFileSync("compress.js")
+console.log("Source length: " + source.length)
 
 var hist, i, max, symbol
 
+console.log("Building histogram...")
 hist = []
 for (i = 0; i < 256; i++) hist[i] = 0
 
@@ -21,13 +24,16 @@ for (i = 0; i < 256; i++) {
 }
 
 var free, table
+
+console.log("Building table...")
 table = new Array(4096)
 for (i = 0; i < table.length; i++) table[i] = []
 
 free = 1
-for (i = 0; i < 4096; i++) {
+for (i = 0; i < table.length; i++) {
   for (symbol = 0; symbol < 256; symbol++) {
-    if (free < table.length && symbol[hist] !== 0 && (symbol % hist[symbol]) === 0) {
+    if (hist[symbol] === 0) continue
+    if (free < table.length && (i % hist[symbol]) === 0) {
       table[free][256] = i
       table[free][257] = symbol
       table[i][symbol] = free++
@@ -35,6 +41,37 @@ for (i = 0; i < 4096; i++) {
   }
 }
 
-for (i = 0; i < table.length; i++) {
-  console.log(table[i].map(x => x === undefined ? " " : ("  " + x).substr(-3)).join(" "))
+var buffer, state
+
+console.log("Encoding...")
+buffer = []
+state = 0
+for (i = 0; i < source.length; i++) {
+  symbol = source[i]
+  if (table[state][symbol] === undefined) {
+    buffer.push(state) // 12 bits!
+    state = 0
+  }
+
+  state = table[state][symbol]
 }
+
+if (state !== 0) buffer.push(state) // 12 bits
+
+console.log("Target length: " + (buffer.length * 1.5))
+
+var copy
+
+console.log("Decoding...")
+copy = []
+for (i = 0; i < buffer.length; i++) {
+  state = buffer[i]
+  while (true) {
+    if (table[state][256] === undefined) break
+    copy.push(table[state][257])
+    state = table[state][256]
+  }
+}
+
+console.log("Copy length: " + copy.length)
+fs.writeFileSync("copy.txt", Buffer.from(copy))
