af5b98a653696e965d73843d8602f526305075b3
Rodin van der Hart <rodin.hart@concentra.co.uk>
2017-04-03 20:20:29 +0100

    Started huffman table

--- huffman/huf.c
+++ huffman/huf.c
@@ -13,34 +13,112 @@ char *read(void) {
   return file;
 }
 
-int *histogram(char *source) {
-  int *hist, symbol;
+typedef struct node {
+  int count;
+  struct node *left;
+  struct node *right;
+  int symbol;
+} node;
 
-  hist = (int *)malloc(256 * sizeof(int));
-  for (symbol = 0; symbol < 256; symbol++) {
-    hist[symbol] = 0;
+node *histogram(char *source) {
+  int symbol;
+  node *hist;
+
+  hist = (node *)malloc(512 * sizeof(node));
+  for (symbol = 0; symbol < 512; symbol++) {
+    hist[symbol].count = 0;
+    hist[symbol].left = NULL;
+    hist[symbol].right = NULL;
+    hist[symbol].symbol = symbol;
   }
 
-  while (symbol = *(source++)) hist[symbol]++;
+  while (symbol = *(source++)) hist[symbol].count++;
 
   return hist;
 }
 
-typedef struct node {
-  
+int compare(void *a, void *b) {
+  node *x, *y;
+
+  x = (node *)a;
+  y = (node *)b;
+
+  if (x->count < y->count) {
+    return -1;
+  } else if (x->count > y->count) {
+    return 1;
+  }
+
+  return 0;
 }
 
-? huftree(int *hist) {
+node *hufftree(node *hist) {
+  int i, n, x, y;
+  node *tmp;
 
+  n = 256;
+  do {
+    x = y = -1;
+    for (i = 0; i < n; i++) {
+      if (hist[i].count < 1) continue;
+
+      if (y == -1) {
+        y = i; // slots x and y available
+      } else {
+        if (x == -1) {
+          if (hist[i].count < hist[y].count) {
+            x = i; // take lower slot x
+          } else {
+            x = y; // shift y to lower slot x
+            y = i;
+          }
+        } else {
+          if (hist[i].count < hist[x].count) {
+            y = x; // shift y to clear lower slot x
+            x = i;
+          } else if (hist[i].count < hist[y].count) {
+            y = i; // replace slot y
+          }
+        }
+      }
+    }
+
+    if (x != -1 && y != -1) {
+      hist[n].count = hist[x].count + hist[y].count;
+      hist[n].left = &hist[x];
+      hist[n].right = &hist[y];
+      n++;
+      hist[x].count = hist[y].count = -1;
+    }
+  } while (x != -1 && y != -1);
+
+  return &hist[y];
+}
+
+void print(node *n) {
+  if (n->left != NULL) {
+    // node
+    printf("(");
+    print(n->left);
+    printf(", ");
+    print(n->right);
+    printf(")");
+  } else {
+    // leaf
+    printf("%d", n->symbol);
+  }
 }
 
 int main(int argc, char *argv[]) {
   char *source;
-  int *hist, symbol;
+  int i;
+  node *hist, *tree;
 
   source = read();
   hist = histogram(source);
   tree = hufftree(hist);
+
+  print(tree);
 
   return 0;
 }
